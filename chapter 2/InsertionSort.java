7c97440e4b9e7a36ab8497e5b631da3e1edbbb16
Yannick Loic <ngapmennyamiyannickloic@gmail.com>
2022-01-03 00:04:26 +0100

    f

--- chapter 2/InsertionSort.java
+++ chapter 2/InsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * @author Nyami yannick (ngapmennyamiyannickloic@gmail.com)
---
+++ chapter 2/MergeSort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] A = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        mergeSort(A);
+        System.out.println(Arrays.toString(A));
+    }
+
+    static int[] mergeSort(int[] A) {
+        sort(A, 0, A.length - 1);
+        return A;
+    }
+
+    static void sort(int[] A, int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(A, l, m);
+            sort(A, m + 1, r);
+            merge(A, l, m, r);
+        }
+    }
+
+    // first array A[l , m]
+    // second array A[m+1 , r]
+    static void merge(int[] A, int l, int m, int r) {
+        int s1 = m - l + 1;
+        int s2 = r - m;
+        int[] L = new int[s1];
+        int[] R = new int[s2];
+        for (int i = 0; i < s1; i++)
+            L[i] = A[l + i];
+        for (int i = 0; i < s2; i++)
+            R[i] = A[m + 1 + i];
+
+        int i = 0, j = 0;
+        int k = l;
+
+        while (i < s1 && j < s2) {
+            if (L[i] < R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < s1) {
+            A[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < s2) {
+            A[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+}
