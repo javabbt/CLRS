fa6a75267aba88fdcae53c7ff03a668823584f26
Yannick Loic <ngapmennyamiyannickloic@gmail.com>
2022-01-02 02:24:23 +0100

    Added decreasing insertion sort

--- chapter 2/InsertionSort.java
+++ chapter 2/InsertionSort.java
@@ -9,13 +9,17 @@ import java.util.Arrays;
 class InsertionSort {
     public static void main(String[] args) {
         int[] A = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-        insertionSort(A);
+        insertionSortAscending(A);
         System.out.println(Arrays.toString(A));
+
+        int[] B = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        insertionSortDescending(B);
+        System.out.println(Arrays.toString(B));
 
     }
 
-    // method that implements the insertion sort
-    static int[] insertionSort(int[] A) {
+    // method that implements the insertion sort in ascending order
+    static int[] insertionSortAscending(int[] A) {
         for (int j = 1; j < A.length; j++) {
             int key = A[j];
             int i = j - 1;
@@ -27,4 +31,18 @@ class InsertionSort {
         }
         return A;
     }
+
+    // method that implements Insertion sort in descending order
+    static void insertionSortDescending(int[] A) {
+        for (int j = 1; j < A.length; j++) {
+            int key = A[j];
+            int i = j - 1;
+            while (i >= 0 && A[i] < key) {
+                A[i + 1] = A[i];
+                i--;
+            }
+            A[i + 1] = key;
+        }
+    }
+
 }
